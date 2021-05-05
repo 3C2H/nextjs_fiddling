4d57364798e31d47ef75aa88548d2fbfb7330099
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-05-05 09:07:51 +0200

    use styles from module

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,3 +1,6 @@
+import styles from '../../components/layout.module.css'
+
+
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return <div className={styles.container}>{children}</div>
 }
