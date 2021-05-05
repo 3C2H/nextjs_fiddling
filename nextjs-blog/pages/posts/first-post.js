3f227d9aa6649c0c6828985b60d239195f95fdf6
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-05-05 09:02:19 +0200

    import layout

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,22 +1,25 @@
 import Link from 'next/link'
 import ImgProfile from '../home'
-
 import Head from 'next/head'
+import layout from '../../components/layout'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Nextjs Post</title>
-      </Head>
-      
-      <h1>First Post</h1>
-      <p>{ImgProfile}</p>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Nextjs Post</title>
+        </Head>
+
+        <h1>First Post</h1>
+          <p>{ImgProfile}</p>
+          <h2>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </h2>
+      </Layout>
     </>
   )
 }
