92feac11d55db59a9e303e658db9a796b7cb7962
3c2h <Charles_Chiacchia_student2018@wilder.school>
2021-05-07 11:02:52 +0200

    update posts title css

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,17 +1,20 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
 
 
 export default function Post({ postData }) {
   return (
   <Layout>
-  {postData.title}
-  <br />
-  {postData.id}
-  <br />
-  {postData.date}
-  <br />
-  <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+    <br />
+    {postData.id}
+    <br />
+    {postData.date}
+    <br />
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
   </Layout>
 )}
 
